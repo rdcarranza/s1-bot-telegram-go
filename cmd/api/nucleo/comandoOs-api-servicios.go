ebd13a87244c1c5ba91fc2b742c7e3bac71b706e
Dario Carranza <dario_zmd@yahoo.com.ar>
2025-01-02 01:50:46 -0300

    Corrección de errores, pero la arquitectura no está bien implementada.
    Debo volver a leer y replantear la funcionalidad.

--- cmd/api/nucleo/comandoOs-api-servicios.go
+++ cmd/api/nucleo/comandoOs-api-servicios.go
@@ -1,11 +1,17 @@
 package nucleo_api
 
+import "github.com/rdcarranza/s1-bot-telegram-go/src/nucleo/servicios"
+
 type ComandoOs_api_servicios struct {
-	comandoOs_api ComandoOs_api
+	cOs_api ComandoOs_api
 }
 
-func NuevoComandoOs_api_servicio(comandoOs_api ComandoOs_api) *ComandoOsServ {
-	return &ComandoOs_api_servicios{
-		comandoOs_api: comandoOs_api,
-	}
+func NuevoComandoOs_api_servicio(comandoOs_api ComandoOs_api, c string) *ComandoOs_api_servicios {
+	comandoOs_api.NuevoComandoOs(c)
+	return &ComandoOs_api_servicios{cOs_api: comandoOs_api}
+}
+
+func (cOs *ComandoOs_api_servicios) EjecutarComandoOs_api_servicio(comandoOs_api ComandoOs_api) (string, error) {
+	servicio := &servicios.ComandoOsServ{}
+	return servicio.EjecutarComando(comandoOs_api.comando)
 }
